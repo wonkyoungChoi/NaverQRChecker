910f0a4386e706441feb0a6c6f3e0b4af181a5c4
wonkyoungChoi <72801820+wonkyoungChoi@users.noreply.github.com>
2022-01-06 03:03:20 +0900

    NaverQRCheck 어플리케이션 프로젝트 로고 변경 더욱 간단하게 수정 네이버 앱과 연동되도록

--- app/src/main/java/com/qr/naverqrcheck/MainActivity.kt
+++ app/src/main/java/com/qr/naverqrcheck/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.qr.naverqrcheck
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabsIntent
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var baseUrl: String
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        baseUrl = "https://nid.naver.com/login/privacyQR"
-
-        val builder = CustomTabsIntent.Builder()
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(this, Uri.parse(baseUrl))
-        finish()
+        try {
+            val url = "naversearchapp://opennadot?cardId=QRCheckIn&character=brown&nclick=nappwiq.qrcheckin&referer=widget&version=46"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+            finish()
+        } catch (e : Exception) {
+            Toast.makeText(applicationContext, "네이버 앱을 설치하셔야 QR인증이 가능합니다.", Toast.LENGTH_SHORT).show()
+            val url = "https://play.google.com/store/apps/details?id=com.nhn.android.search"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+            finish()
+        }
     }
 }
